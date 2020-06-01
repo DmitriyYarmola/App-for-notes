5e8a6f82d30684a1adb0b9d0cae035ec00f96f20
Dmitriy <dmitriy.yarmola@gmail.com>
2020-06-01 12:56:40 +0300

    Updates

---
+++ precache-manifest.aa1e74a9e835b5d9e4ae38a02d9199ca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef4798a9352e6ccc333d301f43a10cfd",
+    "url": "/EnemyDeaD/App-for-notes/index.html"
+  },
+  {
+    "revision": "160f1ecf67be5d6c809f",
+    "url": "/EnemyDeaD/App-for-notes/static/css/2.0b589c22.chunk.css"
+  },
+  {
+    "revision": "6c4a9473f58a6b67c670",
+    "url": "/EnemyDeaD/App-for-notes/static/css/main.c81e6127.chunk.css"
+  },
+  {
+    "revision": "160f1ecf67be5d6c809f",
+    "url": "/EnemyDeaD/App-for-notes/static/js/2.3f8ff7de.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/EnemyDeaD/App-for-notes/static/js/2.3f8ff7de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c4a9473f58a6b67c670",
+    "url": "/EnemyDeaD/App-for-notes/static/js/main.20ac477c.chunk.js"
+  },
+  {
+    "revision": "f6c4404fb353f558c8af",
+    "url": "/EnemyDeaD/App-for-notes/static/js/runtime-main.d20e3480.js"
+  }
+]);
