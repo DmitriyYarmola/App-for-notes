fa241f1bb02203807b52406b2e5e18674403d32b
Dmitriy <dmitriy.yarmola@gmail.com>
2020-06-01 13:36:14 +0300

    Updates

---
+++ precache-manifest.8214f3f57958c6f3e065b3424a90ee69.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f45fd2c26e7d57546d875d7a401f06d",
+    "url": "/App-for-notes/index.html"
+  },
+  {
+    "revision": "322341ace945f7b5f1a9",
+    "url": "/App-for-notes/static/css/2.0b589c22.chunk.css"
+  },
+  {
+    "revision": "a754ea729f31d552615a",
+    "url": "/App-for-notes/static/css/main.c81e6127.chunk.css"
+  },
+  {
+    "revision": "322341ace945f7b5f1a9",
+    "url": "/App-for-notes/static/js/2.68fb146d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/App-for-notes/static/js/2.68fb146d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a754ea729f31d552615a",
+    "url": "/App-for-notes/static/js/main.2e5006d9.chunk.js"
+  },
+  {
+    "revision": "1235f189124a4db19d33",
+    "url": "/App-for-notes/static/js/runtime-main.7d528f94.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0761be466d6df033a9afaa3aa6e44ed2.js"
+  "/App-for-notes/precache-manifest.8214f3f57958c6f3e065b3424a90ee69.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/App-for-notes/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
